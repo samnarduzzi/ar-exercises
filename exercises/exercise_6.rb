86721d3349c1b4a251dd311089ed706bb058a404
Sam <sammynarduzzi@gmail.com>
2023-03-06 20:08:07 -0800

    complete ex6 - one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,16 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
-# Your code goes here ...
+class Store < ActiveRecord::Base
+  has_many :employees
+end
+
+class Employee < ActiveRecord::Base
+  belongs_to :store
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "John", last_name: "Smith", hourly_rate: 80)
+@store1.employees.create(first_name: "Jane", last_name: "Davids", hourly_rate: 75)
+@store2.employees.create(first_name: "Mary", last_name: "Jones", hourly_rate: 90)
+@store2.employees.create(first_name: "Bob", last_name: "Williams", hourly_rate: 70)
