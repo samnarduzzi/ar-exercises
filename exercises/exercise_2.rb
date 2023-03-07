59d9dc076beb0cfb97bf70404050615861faf0e0
Sam <sammynarduzzi@gmail.com>
2023-03-06 18:55:02 -0800

    complete ex2 - update the first store name

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -4,4 +4,9 @@ require_relative './exercise_1'
 puts "Exercise 2"
 puts "----------"
 
-# Your code goes here ...
+@store1 = Store.find(1)
+@store1 = Store.find(2)
+
+@store1.update(name: 'Vancouver')
+
+puts @store1.name
