6718141bbe33eecf9e5f040ec75ce446d018ce50
Sam <sammynarduzzi@gmail.com>
2023-03-06 19:30:18 -0800

    complete ex5 - calculations of annual revenue with diff conditions

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,11 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
-# Your code goes here ...
+total_revenue = Store.sum(:annual_revenue)
+puts "Total Revenue: #{total_revenue}"
+
+average_revenue = Store.average(:annual_revenue)
+puts "Average Revenue: #{average_revenue}"
+
+num_stores_over_1M = Store.where('annual_revenue >= ?', 1000000).count
+puts "Number of stores generating $1M or more in annual revenue sales: #{num_stores_over_1M}"
